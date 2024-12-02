25b367605f718991e2daa3bda866f0f3fde0b425
James Poulten <jpoulten@me.com>
2024-12-02 23:50:58 +0000

    Day 2 solution

--- day2.py
+++ day2.py
@@ -5,62 +5,11 @@
 # Safe rate if 1 < x < 3
 #
 
-def part_1(levels_array):
-    x = []
-    for line in levels_array:
-        l = True
-
-        if line[-1] == line[0]:
-            l = False
-
-        if line[-1] > line[0]:
-            for i in range(len(line)-1):
-                diff = line[i] - line[i+1]
-                if diff < -3 or diff > -1:
-                    l = False
-                    break
-        
-        if line[-1] < line[0]:
-            for i in range(len(line)-1):
-                diff = line[i] - line[i+1]
-                if diff not in range(1, 3): #diff > 3 or diff < 1:
-                    l = False
-                    break
-        x.append(l)
-    
-    return sum(x)
-
-def part_2(levels_array):
-    x = []
-    for line in levels_array:
-        print(line)
-        l = False
-        if line[-1] == line[0]:
-            l = False
-        
-        damper = 0
-        print('d - ', damper)
-        if line[-1] > line[0]:
-            for i in range(len(line)-1):
-                diff = line[i] - line[i+1]
-                print(diff)
-                if diff < -3 or diff > -1:
-                    damper += 1
-        
-        if line[-1] < line[0]:
-            for i in range(len(line)-1):
-                diff = line[i] - line[i+1]
-                print(diff)
-                if diff > 3 and diff < 1:
-                    damper += 1                    
-    
-        if damper > 2:
-            l = True
-        print(damper, l)
-        
-        x.append(l)
-
-    return sum(x)
+def safe(level):
+    diff = [level[i] - level[i+1] for i in range(len(level)-1)]
+    asc = all(map(lambda num: num in range(-3, 0), diff))
+    dec = all(map(lambda num: num in range(1, 4), diff))
+    return asc or dec
 
 def main():
 
@@ -69,11 +18,16 @@ def main():
     for i, l in enumerate(levels):
         levels[i] = [int(ll) for ll in l.split()]
 
-    p1 = part_1(levels)
-    p2 = part_2(levels)
-    print(p1, p2)
+    c1, c2 = 0,0
+    for l in levels:
+        if safe(l):
+            c1 +=1
+        if any(safe(l[:i] + l[i+1:]) for i in range(len(l))):
+            c2 +=1
+    
+    print('Part 1 = ', c1)
+    print('Part 2 = ', c2)
     
     return 
-
 if __name__ == "__main__":
     main()
