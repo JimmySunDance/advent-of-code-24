6ca18e80e51a0745645e11b7b580881eff7768be
James Poulten <37104354+JimmySunDance@users.noreply.github.com>
2024-12-03 16:30:54 +0000

    Day 3 data

---
+++ day3.py
@@ -0,0 +1,15 @@
+# Day three of the Advent of Code 2024
+# 
+# Extract valid "mul(X,Y)" statements, multiply the values and sum the results
+#
+#
+
+
+def main():
+    f = open("data/d3_corrupt_memory_r.txt", "r", encoding="utf-8").read()
+    print(f)
+
+    return 
+
+if __name__ == "__main__":
+    main()
