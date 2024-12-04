b4c3ac9452746d600c7aac25936a014c01a0d8ca
James Poulten <jpoulten@me.com>
2024-12-04 23:15:22 +0000

    Day 3 part 1

--- day3.py
+++ day3.py
@@ -5,23 +5,19 @@
 #
 import re
 
+def part_1(input_string):
+    total = 0
+    for match in re.findall(r'mul\(\d{1,3},\d{1,3}\)', input_string):
+        m = match[4:-1].split(',')
+        total += int(m[0])*int(m[1])
+    return total 
+
 def main():
     f = open("data/d3_corrupt_memory_r.txt", "r", encoding="utf-8").read()
-    mul_split = f.split('mul(')
-    filter1 = [c for c in mul_split if c[0].isnumeric()]
-    
-    a = []
-    for f in filter1:
-        f = f.split(')')
-        a.append(f[0])
-    a = [i for i in a if i[-1].isnumeric()]
-    
-    b = []
-    for i in a:
-        i = i.split(',')
-        b.append(int(i[0]) * int(i[1]))
 
-    print('Part 1: ', sum(b))
+    total = part_1(f)
+    print('Part 1: ', total)
+
     return 
 
 if __name__ == "__main__":
