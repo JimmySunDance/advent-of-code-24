29c8324e1353491fefbef090f8c5594a97871fac
James Poulten <jpoulten@me.com>
2024-12-04 23:41:20 +0000

    Part 2

--- day3.py
+++ day3.py
@@ -12,11 +12,26 @@ def part_1(input_string):
         total += int(m[0])*int(m[1])
     return total 
 
+def part_2(input_string):
+    on, total = True, 0
+    for match in re.findall(r"do\(\)|don't\(\)|mul\(\d{1,3},\d{1,3}\)", input_string):
+        if match == "do()":
+            on = True
+        elif match == "don't()":
+            on = False
+        elif on:
+            m = match[4:-1].split(',')
+            total += int(m[0])*int(m[1])
+
+    return total
+
 def main():
     f = open("data/d3_corrupt_memory_r.txt", "r", encoding="utf-8").read()
 
-    total = part_1(f)
-    print('Part 1: ', total)
+    t1 = part_1(f)
+    t2 = part_2(f)
+    print('Part 1: ', t1)
+    print('Part 2: ', t2)
 
     return 
 
