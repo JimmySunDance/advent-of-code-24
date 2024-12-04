8c82597a9c26850f3caefe865c9b3d33a0783000
James Poulten <37104354+JimmySunDance@users.noreply.github.com>
2024-12-04 19:27:34 +0000

    Day 3 latest

--- day3.py
+++ day3.py
@@ -3,12 +3,25 @@
 # Extract valid "mul(X,Y)" statements, multiply the values and sum the results
 #
 #
-
+import re
 
 def main():
     f = open("data/d3_corrupt_memory_r.txt", "r", encoding="utf-8").read()
-    print(f)
+    mul_split = f.split('mul(')
+    filter1 = [c for c in mul_split if c[0].isnumeric()]
+    
+    a = []
+    for f in filter1:
+        f = f.split(')')
+        a.append(f[0])
+    a = [i for i in a if i[-1].isnumeric()]
+    
+    b = []
+    for i in a:
+        i = i.split(',')
+        b.append(int(i[0]) * int(i[1]))
 
+    print('Part 1: ', sum(b))
     return 
 
 if __name__ == "__main__":
