09b15e43c7bd9ecc337c2ce3b0f25f4698a63971
James Poulten <jpoulten@me.com>
2024-12-12 11:11:29 +0000

    Day 6 solution

--- day6.py
+++ day6.py
@@ -1,62 +1,72 @@
 # Day six of the Advent of Code 2024
 # 
+# Elf in a room
+# 
 
-def part_1(room, pos, facing):
+def part_1(room, r, c):
     h, w = len(room), len(room[0])
+    dr, dc = -1, 0
 
-    travelled = [pos]
-    while pos[0] in range(0, h-1) and pos[1] in range(0, w-1):
-        if facing == "^":
-            n_pos = (pos[0]-1, pos[1])
-            if room[n_pos[0]][n_pos[1]] == "#":
-                facing = ">"
-            else: 
-                pos = n_pos
-                if pos[0] in range(0, h-1) and pos[1] in range(0, w-1):
-                    travelled.append(pos)
-            
-        if facing == ">":
-            n_pos = (pos[0], pos[1]+1)
-            if room[n_pos[0]][n_pos[1]] == "#":
-                facing = "v"
-            else: 
-                pos = n_pos
-                if pos[0] in range(0, h-1) and pos[1] in range(0, w-1):
-                    travelled.append(pos)
-            
-        if facing == "v":
-            n_pos = (pos[0]+1, pos[1])
-            if room[n_pos[0]][n_pos[1]] == "#":
-                facing = "<"
-            else: 
-                pos = n_pos
-                if pos[0] in range(0, h-1) and pos[1] in range(0, w-1):
-                    travelled.append(pos)
-            
-        if facing == "<":
-            n_pos = (pos[0], pos[1]-1)
-            if room[n_pos[0]][n_pos[1]] == "#":
-                facing = "^"
-            else: 
-                pos = n_pos
-                if pos[0] in range(0, h-1) and pos[1] in range(0, w-1):
-                    travelled.append(pos)
+    seen = set()
+    while True:
+        seen.add((r, c))
+        if r+dr < 0 or r+dr >= h or  c+dc < 0 or c+dc >= w: break
+        if room[r + dr][c + dc] == '#':
+            dr, dc = dc, -dr  # assigned at the same time
+        else:
+            r += dr
+            c += dc
 
-    return len(set(travelled))
+    return len(seen)
+
+
+def will_it_loop(room, r, c):
+    rows, cols = len(room), len(room[0])
+    dr, dc = -1, 0
+    seen = set()
+    while True:
+        seen.add((r, c, dr, dc))
+        if r+dr < 0 or r+dr >= rows or c+dc < 0 or c+dc >= cols: 
+            return False
+        if room[r + dr][c + dc] == '#':
+            dr, dc = dc, -dr 
+        else:
+            r += dr
+            c += dc
+        if (r, c, dr, dc) in seen:
+            return True
+
+def part_2(room, r, c):
+    count = 0
+    for cr in range(len(room)):
+        for cc in range(len(room[0])):
+            if room[cr][cc] != '.': continue
+            room[cr][cc] = '#'
+            if will_it_loop(room, r, c):
+                count += 1
+            room[cr][cc] = '.'
+
+    return count
+
+
 
 def main():
     room = open("data/d6_elf_patrol_r.txt", "r", encoding="utf-8").read()
-    room = [l for l in room.splitlines()]
+    room = [list(l) for l in room.splitlines()]
 
     for r in range(len(room)):
         for c in range(len(room[0])):
-            if room[r][c] in "^>v<":
-                facing = room[r][c]
-                pos = (r, c)
+            if room[r][c] == "^":
+                break
+        else:
+            continue
+        break
 
-    p1 = part_1(room, pos, facing)
-
+    p1 = part_1(room, r, c)
     print("Part 1: ", p1)
+
+    p2 = part_2(room, r, c)
+    print("Part 2: ", p2)
 
     return 
 
