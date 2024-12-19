bf7dbed03de2678d856b959c6aa2aca40d078804
James Poulten <jpoulten@me.com>
2024-12-19 13:39:30 +0100

    Part 2 solution

--- day7.py
+++ day7.py
@@ -9,13 +9,29 @@ def can_obtain(t, v):
     if t > v[-1] and can_obtain(t - v[-1], v[:-1]): return True
     return False
 
+def can_concat(t, v):
+    if len(v) == 1: return t == v[0]
+    if t % v[-1] == 0 and can_concat(t // v[-1], v[:-1]): return True
+    if t > v[-1] and can_concat(t - v[-1], v[:-1]): return True
+    sT, sV = str(t), str(v[-1])
+    if len(sT) > len(sV) and \
+        sT.endswith(sV) and \
+        can_concat(int(sT[:-len(sV)]), v[:-1]): 
+        return True
+    return False
 
 def part_1(targets, values):
     total = 0
     for t, v in zip(targets, values):
         if can_obtain(t, v):
             total += t
-    
+    return total
+
+def part_2(targets, values):
+    total = 0
+    for t, v in zip(targets, values):
+        if can_concat(t, v):
+            total += t
     return total
 
 def main():
@@ -29,6 +45,9 @@ def main():
     
     p1 = part_1(targets, vals)
     print("Part 1: ", p1)
+
+    p2 = part_2(targets, vals)
+    print("Part 2: ", p2)
 
     return 
 
